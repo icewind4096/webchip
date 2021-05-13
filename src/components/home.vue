f40867b978a1d47434513887b31f40893e306ecc
Administrator <icewind4096@gmail.com>
2021-05-14 07:57:45 +0800

    完成主框架布局

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-    home component
-  </div>
+  <el-container class="homeContainer">
+    <!-- 头部区域 -->
+    <el-header>Header</el-header>
+    <!-- 页面主题区域 -->
+    <el-container>
+      <!-- 左侧边栏区域 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右侧主题区域 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -9,4 +17,20 @@
 </script>
 
 <style lang="less" scoped>
+.homeContainer{
+  height: 100%;
+}
+
+.el-header{
+  background-color: #373d41;
+}
+
+.el-aside{
+  background-color: #333744;
+}
+
+.el-main{
+  background-color: #EAEDF1;
+}
+
 </style>
