0566e3b8b1881ca3813363d8e9bcef5681923beb
Administrator <icewind4096@gmail.com>
2021-05-15 13:53:14 +0800

    axios时，在header部分加入Authorization属性值

--- src/main.js
+++ src/main.js
@@ -10,6 +10,12 @@ import './assets/css/global.css'
 // 导入axios
 import axios from 'axios'
 axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+
+axios.interceptors.request.use(config => {
+  // 必须return config
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
